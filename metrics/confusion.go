95db85d5ceb5a2b01fa857dafca1ba6d8efd3a68
NathanC <nathas10.alves@gmail.com>
2026-06-14 16:24:18 -0300

    feat: add confusion matrix metrics

--- metrics/confusion.go
+++ metrics/confusion.go
@@ -1,1 +1,65 @@
 package metrics
+
+type ConfusionMatrix struct {
+	TruePositive  int
+	TrueNegative  int
+	FalsePositive int
+	FalseNegative int
+}
+
+func NewConfusionMatrix() ConfusionMatrix {
+	return ConfusionMatrix{}
+}
+
+func (m *ConfusionMatrix) Add(yHat, y, threshold float64) {
+	predicted := Classify(yHat, threshold)
+	actual := int(y)
+
+	switch {
+	case predicted == 1 && actual == 1:
+		m.TruePositive++
+	case predicted == 0 && actual == 0:
+		m.TrueNegative++
+	case predicted == 1 && actual == 0:
+		m.FalsePositive++
+	case predicted == 0 && actual == 1:
+		m.FalseNegative++
+	}
+}
+
+func (m ConfusionMatrix) Total() int {
+	return m.TruePositive + m.TrueNegative + m.FalsePositive + m.FalseNegative
+}
+
+func (m ConfusionMatrix) Correct() int {
+	return m.TruePositive + m.TrueNegative
+}
+
+func (m ConfusionMatrix) Accuracy() float64 {
+	return Accuracy(m.Correct(), m.Total())
+}
+
+func (m ConfusionMatrix) Precision() float64 {
+	denominator := m.TruePositive + m.FalsePositive
+	if denominator == 0 {
+		return 0
+	}
+	return float64(m.TruePositive) / float64(denominator)
+}
+
+func (m ConfusionMatrix) Recall() float64 {
+	denominator := m.TruePositive + m.FalseNegative
+	if denominator == 0 {
+		return 0
+	}
+	return float64(m.TruePositive) / float64(denominator)
+}
+
+func (m ConfusionMatrix) F1() float64 {
+	precision := m.Precision()
+	recall := m.Recall()
+	if precision+recall == 0 {
+		return 0
+	}
+	return 2 * precision * recall / (precision + recall)
+}
